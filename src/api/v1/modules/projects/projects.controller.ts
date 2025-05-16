9053800b29a0800f9ea58c1a11201c4684b46ae5
the-geniustech <holumidey70@gmail.com>
2025-05-16 10:05:02 -0700

    Bug fixing-3

--- src/api/v1/modules/projects/projects.controller.ts
+++ src/api/v1/modules/projects/projects.controller.ts
@@ -8,65 +8,66 @@ import { sendSuccess } from "@utils/responseHandler";
 import { IProject } from "@interfaces/project.interface";
 import { search } from "@utils/search.util";
 import { AdvancedAPIFeatures, APIFeatures } from "@utils/apiFeatures.util";
+import Scope from "@models/Scope.model";
 import Project from "@models/Project.model";
 import AppError from "@utils/appError";
 import { getInitials } from "@utils/getInitials.util";
 
-// export const createProject = catchAsync(async (req: Request, res: Response) => {
-//   const user = req.user;
-//   if (!user) {
-//     throw new AppError("User not found", 404);
-//   }
+export const createProject = catchAsync(async (req: Request, res: Response) => {
+  const user = req.user;
+  if (!user) {
+    throw new AppError("User not found", 404);
+  }
 
-//   const scope = await getScopeById(req.body.scopeId);
-//   if (!scope || scope.deletedAt || !scope.client) {
-//     throw new AppError("Scope or Client could not be found", 404);
-//   }
+  const scope = await Scope.findById(req.body.scopeId).populate("client");
+  if (!scope || scope.deletedAt || !scope.client) {
+    throw new AppError("Scope or Client could not be found", 404);
+  }
 
-//   if (
-//     !scope.client ||
-//     typeof scope.client !== "object" ||
-//     !("clientNature" in scope.client) ||
-//     !("clientName" in scope.client) ||
-//     !("_id" in scope.client)
-//   ) {
-//     throw new AppError("Invalid client data in scope", 500);
-//   }
+  if (
+    !scope.client ||
+    typeof scope.client !== "object" ||
+    !("clientNature" in scope.client) ||
+    !("clientName" in scope.client) ||
+    !("_id" in scope.client)
+  ) {
+    throw new AppError("Invalid client data in scope", 500);
+  }
 
-//   const client = scope.client as {
-//     _id: string;
-//     clientName: string;
-//     clientNature: string;
-//   };
+  const client = scope.client as {
+    _id: string;
+    clientName: string;
+    clientNature: string;
+  };
 
-//   const projectId = await projectService.generateNextProjectId(
-//     getInitials(scope.scopeTitle)
-//   );
+  const projectId = await projectService.generateNextProjectId(
+    getInitials(scope.scopeTitle)
+  );
 
-//   const payload = validateData(createProjectSchema, {
-//     ...req.body,
-//     projectId,
-//     title: scope.scopeTitle,
-//     type: client.clientNature,
-//     category: scope.natureOfWork,
-//     client: client._id.toString(),
-//     clientName: client.clientName,
-//     scope: scope.id,
-//     createdBy: user.id,
-//   });
+  const payload = validateData(createProjectSchema, {
+    ...req.body,
+    projectId,
+    title: scope.scopeTitle,
+    type: client.clientNature,
+    category: scope.natureOfWork,
+    client: client._id.toString(),
+    clientName: client.clientName,
+    scope: scope.id,
+    createdBy: user.id,
+  });
 
-//   const project = await projectService.createProject(
-//     payload as Partial<IProject>,
-//     user
-//   );
+  const project = await projectService.createProject(
+    payload as Partial<IProject>,
+    user
+  );
 
-//   return sendSuccess({
-//     res,
-//     statusCode: 201,
-//     message: "Project created successfully",
-//     data: { project },
-//   });
-// });
+  return sendSuccess({
+    res,
+    statusCode: 201,
+    message: "Project created successfully",
+    data: { project },
+  });
+});
 
 export const getProject = catchAsync(async (req: Request, res: Response) => {
   const { id } = req.params;
