bb6aef5e6c84c215b117931ed2b0871f8a170d1d
the-geniustech <holumidey70@gmail.com>
2025-05-16 10:37:06 -0700

    Bug fixing-5

--- src/interfaces/scope.interface.ts
+++ src/interfaces/scope.interface.ts
@@ -4,6 +4,26 @@ export type ScopeStatus = "pending" | "approved" | "rejected";
 export type ScopeSource = "manual" | "client_upload" | "AI";
 
 export interface IScope {
+  scopeId: string;
+  client: ObjectId;
+  scopeTitle: string;
+  scopeOverview: string;
+  entryRequirements: string[];
+  natureOfWork: string;
+  isUploadedScopes: boolean;
+  uploadedScopes: object[];
+  progress: number;
+  status: "pending" | "approved" | "rejected";
+  rejectionReason?: string | null;
+  rejectionMessage?: string | null;
+  source: "manual" | "client_upload" | "AI";
+  addedBy: ObjectId;
+  deletedAt?: Date | null;
+}
+
+export interface IScopeDocument extends IScope, Document {}
+
+/* export interface IScope {
   client?: ObjectId;
   scopeId: string;
   scopeOverview: string;
@@ -21,7 +41,7 @@ export interface IScope {
   rejectionMessage?: string;
 }
 
-export interface IScopeDocument extends IScope, Document {}
+export interface IScopeDocument extends IScope, Document {} */
 
 export interface ResendScopeApprovalOptions {
   admin: {
