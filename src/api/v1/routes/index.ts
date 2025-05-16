b1653d3b45805d3200d8e38397e3b3859447ba30
the-geniustech <holumidey70@gmail.com>
2025-05-16 09:09:27 -0700

    Updates

--- src/api/v1/routes/index.ts
+++ src/api/v1/routes/index.ts
@@ -4,7 +4,7 @@ import { Router } from "express";
 import authRoutes from "@modules/auth/auth.routes";
 import userRoutes from "@modules/users/users.routes";
 import clientRoutes from "@modules/clients/clients.routes";
-import scopeRoutes from "@modules/scope/scope.routes";
+// import scopeRoutes from "@modules/scope/scope.routes";
 import projectRoutes from "@modules/projects/projects.routes";
 import siteVisitRoutes from "@modules/site-visits/siteVisit.routes";
 
@@ -14,7 +14,7 @@ const router = Router();
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 router.use("/clients", clientRoutes);
-router.use("/scopes", scopeRoutes);
+// router.use("/scopes", scopeRoutes);
 router.use("/projects", projectRoutes);
 router.use("/site-visit", siteVisitRoutes);
 
